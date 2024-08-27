02d6c59fd413616302958f7745a3b3fe84bd7bfa
Casey Milne <casey@saberwp.com>
2024-08-27 21:27:40 +0200

    Setting for changing doc page.

--- lib/Admin.php
+++ lib/Admin.php
@@ -17,17 +17,20 @@ class Admin {
         add_action( 'carbon_fields_register_fields', function() {
             Container::make( 'theme_options', __( 'Docly', 'crb' ) )
                 ->add_fields( array(
-                    Field::make( 'checkbox', 'docly_github_button', 'Render GitHub Button' )->set_default_value( true ),
-                    Field::make( 'text', 'docly_github_url', 'GitHub URL' ),
+                    Field::make( 'select', 'docly_doc_page_id', __('Documentation Page') )
+                        ->add_options( array_combine(
+                            wp_list_pluck( get_pages(), 'ID' ),
+                            wp_list_pluck( get_pages(), 'post_title' )
+                        )),
                     Field::make( 'image', 'docly_logo', 'Logo Upload' ),
                     Field::make('color', 'docly_color_primary', __('Primary Color'))->set_default_value('#FFFFFF'),
                     Field::make('color', 'docly_color_accent', __('Accent Color'))->set_default_value('#0C8CE9'),
                     Field::make('color', 'docly_color_offset', __('Offset Color'))->set_default_value('#737373'),
                     Field::make('color', 'docly_color_text', __('Text Color'))->set_default_value('#242424'),
+                    Field::make( 'checkbox', 'docly_github_button', 'Render GitHub Button' )->set_default_value( true ),
+                    Field::make( 'text', 'docly_github_url', 'GitHub URL' ),
                 ));
-        }); 
-
-        
+        });        
 
     }
 
